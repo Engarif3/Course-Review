a0ce4492e66281af45c0c70631011bb82d43c398
Md Arifur Rahman <arif.aust.eng@gmail.com>
2024-08-28 18:56:41 +0200

    next js setup done

---
+++ Frontend/course-review-frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h2>Hello</h2>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+};
+
+export default page;
