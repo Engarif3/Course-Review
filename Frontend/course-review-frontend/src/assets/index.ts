94c01cddf4ee3b3ffbf472f19333c07561f95655
Md Arifur Rahman <arif.aust.eng@gmail.com>
2024-09-11 16:47:13 +0200

    hero section done

--- Frontend/course-review-frontend/src/assets/index.ts
+++ Frontend/course-review-frontend/src/assets/index.ts
@@ -2,6 +2,10 @@ const assets = {
   images: {
     messenger: require("./icons/messenger.webp"),
     engineering: require("./icons/Engineering.png"),
+    science: require("./icons/science.png"),
+    tech: require("./icons/tech.png"),
+    grid: require("./icons/grid.png"),
+    grid1: require("./icons/grid1.png"),
   },
   svgs: {
     whatsapp: require("./icons/whatsapp.svg"),
--- Frontend/course-review-frontend/src/components/UI/Homepage/HeroSection/HeroSection.tsx
+++ Frontend/course-review-frontend/src/components/UI/Homepage/HeroSection/HeroSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import Image from "next/image";
 import assets from "@/assets";
 
@@ -8,14 +8,71 @@ const HeroSection = () => {
       <Box
         sx={{
           flex: 1,
-          position: "row",
+          position: "relative",
+          zIndex: 1,
         }}
       >
-        <Box>
-          <Image src={assets.svgs.whatsapp} alt="" />
+        <Box
+          sx={{
+            position: "absolute",
+            zIndex: 0,
+            // width: "900px",
+            top: "-90px",
+            left: "-120px",
+            opacity: "60%",
+          }}
+        >
+          <Image src={assets.images.grid1} alt="" />
         </Box>
+        <Typography variant="h3" component="h1" fontWeight={600}>
+          Explore the Knowledge Of
+        </Typography>
+        <Typography variant="h3" component="h1" fontWeight={600}>
+          Deep Ocean
+        </Typography>
+        <Typography
+          color="primary.main"
+          variant="h3"
+          component="h1"
+          fontWeight={600}
+        >
+          With EduQuest
+        </Typography>
+        <Typography
+          variant="h6"
+          component="p"
+          fontWeight={300}
+          sx={{ width: "50%" }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+          doloribus, suscipit saepe eveniet itaque incidunt distinctio, beatae
+          rem sed deleniti praesentium. Inventore quo debitis harum qui
+          molestias sunt expedita corrupti nam accusamus quas, numquam autem
+          perferendis omnis exercitationem.
+        </Typography>
+        <Button>Enroll Now</Button>
+        <Button
+          variant="outlined"
+          sx={{
+            marginTop: "5px",
+            marginLeft: "8px",
+          }}
+        >
+          Contact Us
+        </Button>
       </Box>
-      <Box>Right</Box>
+      <Box
+        sx={{
+          position: "absolute",
+          zIndex: 1,
+          width: "600px",
+          top: "100px",
+          right: "240px",
+          opacity: "15%",
+        }}
+      >
+        <Image src={assets.images.grid} alt="" />
+      </Box>
     </Container>
   );
 };
